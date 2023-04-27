3134c92fde94d489023f61cec9e707995a2cf54f
yjcyxky <yjcyxky@163.com>
2023-04-27 18:30:56 -0400

    First Commit.

---
+++ README.md
@@ -0,0 +1,13 @@
+### How to build index for my own data?
+
+```
+python chatbot.py index -d docs/type2-dm
+```
+
+### Examples
+
+1. What's the Long Covid? which papers mentioned it? Could you list all doi numbers of these papers?
+
+```
+The Long Covid was first mentioned in paper no. 2021, which is titled Incidence, co-occurrence, and evolution of long-COVID features: a 6-month retrospective cohort study of 273,618 survivors of COVID-19. The Long Covid is defined as the symptoms lasting more than 6 months after infection with COVID-19. The papers no. 2021, 2022, 2023, 2024, and 2025 mention the Long Covid and have the doi numbers 10.1007/s10654-022-00847-8, 2015–2016,  J. Am. Med. Assoc.  325, 2015–2016,  and Eur. J. Epidemiol.  https://doi.org/10.1007/s10654-022-00847-8, respectively.
+```
---
+++ chatbot.py
@@ -0,0 +1,96 @@
+import gradio as gr
+import click
+import json
+import os
+from gpt_index import SimpleDirectoryReader, ServiceContext, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+
+os.environ["OPENAI_API_KEY"] = "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"
+
+
+def check_variable():
+    if os.environ.get("OPENAI_API_KEY") is None:
+        print("Please set the OPENAI_API_KEY environment variable.")
+        exit(1)
+
+
+def launch_chatbot(index_filepath="index.json"):
+    index = GPTSimpleVectorIndex.load_from_disk(index_filepath)
+
+    def chatbot(input_text):
+        response = index.query(input_text, response_mode="compact")
+        return response.response.strip()
+
+    return chatbot
+
+
+@click.group()
+def chatbot():
+    pass
+
+
+@chatbot.command(help="Build index from directory of documents.")
+@click.option('--directory-path', '-d', required=True, help="The directory which saved the documents.")
+def index(directory_path):
+    check_variable()
+
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(
+        max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    llm_predictor = LLMPredictor(llm=OpenAI(
+        temperature=0.7, model_name="text-davinci-003", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory_path).load_data()
+
+    service_context = ServiceContext.from_defaults(
+        llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+    doc_index = GPTSimpleVectorIndex.from_documents(
+        documents, service_context=service_context)
+    doc_index = GPTSimpleVectorIndex.from_documents(documents)
+
+    doc_index.save_to_disk(os.path.join(directory_path, 'index.json'))
+    metadata = {
+        "max_input_size": max_input_size,
+        "num_outputs": num_outputs,
+        "max_chunk_overlap": max_chunk_overlap,
+        "chunk_size_limit": chunk_size_limit,
+        "directory_path": directory_path,
+        "index_type": "GPTSimpleVectorIndex",
+        "model_name": "text-davinci-003",
+        "temperature": 0.7,
+        "max_tokens": num_outputs,
+        "index_filepath": os.path.join(directory_path, 'index.json'),
+        "num_documents": len(documents),
+        "document_names": [os.path.basename(file) for file in os.listdir(directory_path)]
+    }
+    with open(os.path.join(directory_path, 'metadata.json'), 'w') as f:
+        json.dump(metadata, f)
+
+    return index
+
+
+@chatbot.command(help="Query index.")
+@click.option('--index-filepath', '-i', required=True, help="The index file path.")
+def query(index_filepath):
+    check_variable()
+
+    if os.path.exists(index_filepath):
+        iface = gr.Interface(fn=launch_chatbot(index_filepath=index_filepath),
+                             inputs=gr.inputs.Textbox(lines=7,
+                                                      label="Enter your text"),
+                             outputs="text",
+                             title="Custom-trained AI Chatbot")
+
+        iface.launch(share=True)
+    else:
+        print("Index file not found.")
+        return
+
+
+if __name__ == "__main__":
+    chatbot()
